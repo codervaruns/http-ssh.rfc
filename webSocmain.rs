d49efbb9e0fb9be40f87f0a00c3b2ae6894f4318
aceqbit <ashwin0637@gmail.com>
2025-08-27 22:17:08 +0530

    webSocket's main file

---
+++ webSocmain.rs
@@ -0,0 +1,22 @@
+mod lobby;
+mod ws;
+use lobby:Lobby;
+mod messages;
+mod start_connection;
+use actix:Actor;
+use start_connection::start_connection as start_connection_route;
+
+use actix_web::{App,HttpServer};
+
+#[actix_web::main]
+asyn fn main()->std::io::Result<()>{
+    let chat_server=Lobby::default().start;
+    HttpServer::new(move||{
+        App::new()
+          .service(start_connection_route)
+          .data(chat_server.clone())
+    })
+    .bind("127.0.0.1:8080")
+    .run()
+    .await
+}
