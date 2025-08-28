cd30777277a9a3a457ab797ab83fd5796735fbed
aceqbit <ashwin0637@gmail.com>
2025-08-28 13:15:44 +0530

    Update message.rs

--- message.rs
+++ message.rs
@@ -1,30 +1,29 @@
-use actix::prelude::{Message,Recipient};
-use Vvid::Vvid;
+use actix::prelude::{Message, Recipient};
+use uuid::Uuid as Vvid; // Assuming Vvid is just Uuid, change if you have your own type
 
-#[derive{message}]
-#[rtype[result="{}"]]
-pub struct WsMessage{pub String};
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct WsMessage(pub String);
 
-#[derive{message}]
-#[rtype[result="{}"]]
-publ struct Connect{
-    pub addr:Recipient<WsMessage>,
-    pub lobby_id:Vvid,
-    pub self_id:Vvid,
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct Connect {
+    pub addr: Recipient<WsMessage>,
+    pub lobby_id: Vvid,
+    pub self_id: Vvid,
 }
 
-#[derive{message}]
-#[rtype[result="{}"]]
-publ struct Disonnect{
-    pub lobby_id:Vvid,
-    pub self_id:Vvid,
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct Disconnect {
+    pub lobby_id: Vvid,
+    pub self_id: Vvid,
 }
 
-#[derive{message}]
-#[rtype[result="{}"]]
-publ struct ClientActorMessage{
-    pub id:Vvid,
-    pub msg:String,
-    pub room_id:Vvid
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct ClientActorMessage {
+    pub id: Vvid,
+    pub msg: String,
+    pub room_id: Vvid,
 }
-
