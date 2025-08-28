a967e56489401444d374e6a0e006177d416326e4
aceqbit <ashwin0637@gmail.com>
2025-08-28 13:18:23 +0530

    Update webSocmain.rs

--- webSocmain.rs
+++ webSocmain.rs
@@ -1,22 +1,24 @@
 mod lobby;
 mod ws;
-use lobby:Lobby;
 mod messages;
 mod start_connection;
-use actix:Actor;
+
+use lobby::Lobby;
+use actix::Actor;
 use start_connection::start_connection as start_connection_route;
 
-use actix_web::{App,HttpServer};
+use actix_web::{App, HttpServer};
 
 #[actix_web::main]
-asyn fn main()->std::io::Result<()>{
-    let chat_server=Lobby::default().start;
-    HttpServer::new(move||{
+async fn main() -> std::io::Result<()> {
+    let chat_server = Lobby::default().start();
+
+    HttpServer::new(move || {
         App::new()
-          .service(start_connection_route)
-          .data(chat_server.clone())
+            .service(start_connection_route)
+            .app_data(actix_web::web::Data::new(chat_server.clone()))
     })
-    .bind("127.0.0.1:8080")
+    .bind("127.0.0.1:8080")?
     .run()
     .await
 }
